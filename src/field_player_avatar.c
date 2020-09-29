96a7ef7583a960c0bd36c2955a20fae7bfc8e832
Evan <eroelke@gmail.com>
2020-09-29 15:44:51 -0600

    reset chain fishing counter on failed reeling

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1958,6 +1958,7 @@ static bool8 Fishing_StartEncounter(struct Task *task)
 
 static bool8 Fishing_NotEvenNibble(struct Task *task)
 {
+    gChainFishingStreak = 0;    //player failed to reel in a mon. reset chain fishing counter
     AlignFishingAnimationFrames();
     StartSpriteAnim(&gSprites[gPlayerAvatar.spriteId], GetFishingNoCatchDirectionAnimNum(GetPlayerFacingDirection()));
     FillWindowPixelBuffer(0, PIXEL_FILL(1));
@@ -1968,6 +1969,7 @@ static bool8 Fishing_NotEvenNibble(struct Task *task)
 
 static bool8 Fishing_GotAway(struct Task *task)
 {
+    gChainFishingStreak = 0;    //player failed to reel in a mon. reset chain fishing counter
     AlignFishingAnimationFrames();
     StartSpriteAnim(&gSprites[gPlayerAvatar.spriteId], GetFishingNoCatchDirectionAnimNum(GetPlayerFacingDirection()));
     FillWindowPixelBuffer(0, PIXEL_FILL(1));
