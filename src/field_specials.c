69bd4378fd3affc959d9fd25cc568c6e231f73cd
LOuroboros <lunosouroboros@gmail.com>
2021-03-02 14:21:55 -0300

    Added Pokémon Adventures themed OW sprites of Brendan and May Thanks to the Chinese hack "Pokémon Shadow Specter".

--- src/field_specials.c
+++ src/field_specials.c
@@ -4378,6 +4378,8 @@ void SwapPlayersCostume(void)
 
     if (gSaveBlock2Ptr->costumeId == 0)
         gSaveBlock2Ptr->costumeId = 1;
+    else if (gSaveBlock2Ptr->costumeId == 1)
+        gSaveBlock2Ptr->costumeId = 2;
     else
         gSaveBlock2Ptr->costumeId = 0;
 
