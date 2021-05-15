5c1262b805758f2ddd034e8a00cb3e037a2f719e
LOuroboros <lunosouroboros@gmail.com>
2021-05-15 20:03:09 -0300

    Replaced the gender icon in battle for shinies with a star icon Thanks to Citrus Bolt.

--- src/battle_interface.c
+++ src/battle_interface.c
@@ -27,6 +27,7 @@
 #include "constants/battle_config.h"
 #include "data.h"
 #include "pokemon_summary_screen.h"
+#include "strings.h"
 
 enum
 {   // Corresponds to gHealthboxElementsGfxTable (and the tables after it) in graphics.c
@@ -2049,15 +2050,24 @@ static void UpdateNickInHealthbox(u8 healthboxSpriteId, struct Pokemon *mon)
     switch (gender)
     {
     default:
-        StringCopy(ptr, gText_DynColor2);
+		if (IsMonShiny(mon))
+			StringCopy(ptr, gText_ShinyStar);
+		else
+			StringCopy(ptr, gText_DynColor2);
         windowTileData = AddTextPrinterAndCreateWindowOnHealthbox(gDisplayedStringBattle, 0, 3, 2, &windowId);
         break;
     case MON_MALE:
-        StringCopy(ptr, gText_DynColor2Male);
+		if (IsMonShiny(mon))
+			StringCopy(ptr, gText_ShinyStarMale);
+		else
+			StringCopy(ptr, gText_DynColor2Male);
         windowTileData = AddTextPrinterAndCreateWindowOnHealthbox(gDisplayedStringBattle, 0, 3, 2, &windowId);
         break;
     case MON_FEMALE:
-        StringCopy(ptr, gText_DynColor1Female);
+		if (IsMonShiny(mon))
+			StringCopy(ptr, gText_ShinyStarFemale);
+		else
+			StringCopy(ptr, gText_DynColor1Female);
         windowTileData = AddTextPrinterAndCreateWindowOnHealthbox(gDisplayedStringBattle, 0, 3, 2, &windowId);
         break;
     }
