6f775c8b57ea535ddb67c088409c24ef7bb27d10
TJLoyd1 <tjloyd2001@gmail.com>
2021-05-29 16:06:08 -0500

    What is This For?

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -540,8 +540,6 @@
 #define PH_NURSE_BLEND              607
 #define PH_NURSE_HELD               608
 #define PH_NURSE_SOLO               609
-#define MUS_TRAINER_BATTLE_DPPT	    610
-#define MUS_VS_TRAINER_COLO			611
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
